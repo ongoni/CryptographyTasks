8652ecfcd5d3d71621ab5d30bbf1f39436157756
ongoni <ongoni.fg@gmail.com>
2017-09-15 18:12:20 +0400

    added bit cipher task

--- CryptographyTasks/Program.cs
+++ CryptographyTasks/Program.cs
@@ -17,14 +17,16 @@ namespace CryptographyTasks {
 
     class Program {
 
+        private static readonly string LoremImpsumPath = "../../lorem ipsum.txt"; //путь к файлу с обычными текстом
+        private static readonly string LoremImpsumLongPath = "../../lorem ipsum (long).txt"; //путь к файлу с длинным текстом
         private static readonly List<char> Alphabet = new List<char> { //латинский алфавит
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-        };
+        }; //латинский алфавит
         
         //первое задание - вычисление контрольной суммы файла
         public static void Task1() {
-            Console.WriteLine("File contol sum - " + GetFileMd5("../../lorem ipsum.txt") + '\n'); //вывод контрольной суммы
+            Console.WriteLine("File contol sum - " + GetFileMd5(LoremImpsumPath) + '\n'); //вывод контрольной суммы
         }
         
         private static string GetFileMd5(string path) {
@@ -42,7 +44,7 @@ namespace CryptographyTasks {
 
         //второе задание, часть 1 - шифр "атбаш"
         public static void Task2_1() {
-            string atbashEncoded = GetAtbashCode(File.ReadAllText("../../lorem ipsum.txt"));
+            string atbashEncoded = GetAtbashCode(File.ReadAllText(LoremImpsumPath));
             string atbashDecoded = GetAtbashCode(atbashEncoded);
             Console.WriteLine("Atbash cipher code:");
             Console.WriteLine("1. encoded - " + atbashEncoded); //вывод закодированных данных
@@ -69,7 +71,7 @@ namespace CryptographyTasks {
 
         //второе задание, часть 2 - шифр Цезаря
         public static void Task2_2() {
-            string caesarEncoded = CaesarCipherEncode(File.ReadAllText("../../lorem ipsum.txt"));
+            string caesarEncoded = CaesarCipherEncode(File.ReadAllText(LoremImpsumPath));
             string caesarDecoded = CaesarCipherDecode(caesarEncoded);
             Console.WriteLine("Caesar cipher code:");
             Console.WriteLine("1. encoded - " + caesarEncoded); //вывод закодированных данных
@@ -144,49 +146,11 @@ namespace CryptographyTasks {
             }
 
             //удаляем лишние символы и вызываем функцию шифрования
-            string routeEncoded = RouteCipherEncode(RemoveSymbols(File.ReadAllText("../../lorem ipsum.txt")), n, m, key);
+            string routeEncoded = RouteCipherEncode(RemoveSymbols(File.ReadAllText(LoremImpsumPath)), n, m, key);
             string routeDecoded = RouteCipherDecode(routeEncoded, n, m, key);
             Console.WriteLine("Route cipher code:");
             Console.WriteLine("1. encoded - " + routeEncoded);
             Console.WriteLine("2. decoded - " + routeDecoded + '\n');
-        }
-        
-        //добавление символов в конец
-        private static string AppendSymbols(string row, int length) {
-            StringBuilder result = new StringBuilder(row); //здесь будет результат
-            while (result.Length != length) { //добавляем в конец символ 'x', пока длина блока не достигнет нужной длины
-                result.Append('x');
-            }
-
-            return result.ToString(); //возвращаем результат
-        }
-
-        //удаление всех символов кроме букв
-        private static string RemoveSymbols(string text) {
-            StringBuilder result = new StringBuilder(text); //здесь будет результат
-            for (int i = 0; i < result.Length; i++) { //пока не прошли все символы
-                if (!Char.IsLetter(result[i])) { //если символ не является буквой, то удаляем его и уменьшаем i
-                    result.Remove(i, 1);
-                    i--;
-                }
-            }
-
-            return result.ToString(); //возвращаем результат
-        }
-
-        //выясняем порядок вывода столбцов
-        private static ArrayList GetShowOrder(string key) {
-            ArrayList result = new ArrayList(); //здесь будет результат
-            string sorted = String.Concat(key.OrderBy(x => x)); //сортируем буквы ключа в алфавитном порядке
-
-            for (int i = 0; i < key.Length; i++) { //добавляем индексы столбцов в массив в нужном порядке
-                //ищем позицию элемента из отсортированных букв ключа в самом ключе
-                //пусть ключ 'qwert', отсортированные буквы ключа - 'eqrtw'
-                //'e' находится на 3 позиции в ключе (в 1-индесации) => первым выводим третий столбец и т.д.
-                result.Add(key.IndexOf(sorted[i]));
-            }
-            
-            return result; //возвращаем результат
         }
         
         //шифрование
@@ -282,7 +246,148 @@ namespace CryptographyTasks {
 
             return result.ToString(); //возвращаем результат
         }
+        
+        //добавление символов в конец
+        private static string AppendSymbols(string row, int length) {
+            StringBuilder result = new StringBuilder(row); //здесь будет результат
+            while (result.Length != length) { //добавляем в конец символ 'x', пока длина блока не достигнет нужной длины
+                result.Append('x');
+            }
 
+            return result.ToString(); //возвращаем результат
+        }
+
+        //удаление всех символов кроме букв
+        private static string RemoveSymbols(string text) {
+            StringBuilder result = new StringBuilder(text); //здесь будет результат
+            for (int i = 0; i < result.Length; i++) { //пока не прошли все символы
+                if (!Char.IsLetter(result[i])) { //если символ не является буквой, то удаляем его и уменьшаем i
+                    result.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            return result.ToString(); //возвращаем результат
+        }
+
+        //выясняем порядок вывода столбцов
+        private static ArrayList GetShowOrder(string key) {
+            ArrayList result = new ArrayList(); //здесь будет результат
+            string sorted = String.Concat(key.OrderBy(x => x)); //сортируем буквы ключа в алфавитном порядке
+
+            for (int i = 0; i < key.Length; i++) { //добавляем индексы столбцов в массив в нужном порядке
+                //ищем позицию элемента из отсортированных букв ключа в самом ключе
+                //пусть ключ 'qwert', отсортированные буквы ключа - 'eqrtw'
+                //'e' находится на 3 позиции в ключе (в 1-индесации) => первым выводим третий столбец и т.д.
+                result.Add(key.IndexOf(sorted[i]));
+            }
+            
+            return result; //возвращаем результат
+        }
+
+        //четвёртное задание
+        public static void Task4() {
+            Console.Write("Enter phrase to encode: ");
+            string data = Console.ReadLine();
+            
+            Console.WriteLine("Bit cipher code:");
+            //вызываем функцию шифрования, сразу отправляя туда биты из двоичного представления данных, которые нужно зашифровать
+            BitCipherEncode(LoremImpsumLongPath, GetBits(data)); 
+            string bitCipherDecoded = BitCipherDecode(LoremImpsumLongPath);
+            Console.WriteLine("Decoded phrase - " + bitCipherDecoded + '\n');
+        }
+        
+        //шифрование
+        //после шифрования дааные в файле переписываются вместе с зашифрованными данными
+        public static void BitCipherEncode(string path, string data) {
+            StringBuilder result = new StringBuilder(); //здесь будет результат
+            //из входного файла считываем весь текст, разделив его на части по переводу строки, т.е. текст
+            //привет мир,
+            //я вернулся!
+            //разделится на две части - "привет мир," и "я вернулся!"
+            string[] text = File.ReadAllText(path).Split('\n'); 
+            for (int i = 0; i < text.Length; i++) { 
+                //для каждой части текста проверяем наличие пробела в конце строки
+                if (text[i].EndsWith(" ")) {
+                    //если пробел в конце есть, то удаляем его, чтобы он не помешал при дешифровании
+                    //т.е. обрезаем строку с 0-го символа по предпоследний
+                    text[i] = text[i].Substring(0, text[i].Length - 1); 
+                }
+            }
+            
+            for (int i = 0; i < data.Length; i++) { //для каждого бита из фразы, которую нужно зашифровать
+                if (data[i] == '1') { //если бит равен 1, то добавляем пробел
+                    text[i] += ' ';
+                } //иначе ничего не делаем
+            }
+            
+            //добавляем перевод строки каждой строке, чтобы записать текст в исходном виде,
+            //но с уже зашифрованными данными
+            foreach (string s in text) { 
+                result.Append(s + '\n'); //добавляем текущую строку, прибавив перевод строки
+            }
+
+            File.WriteAllText(path, result.ToString()); //заполняем файл по указанном пути path полученными данными result
+            
+            Console.WriteLine("Secret phrase encoded to file " + path); //выводим сообщение о том, что данные зашифрованы
+        }
+        
+        //дешифрование
+        //после дешифрования файл с зашифрованными данными не изменяется, сами зашифрованные данные возвращаются в виде строки типа string
+        public static string BitCipherDecode(string path) {
+            StringBuilder bits = new StringBuilder(); //здесь будут биты, полученные из текста с зашифрованными данными
+            //из входного файла считываем весь текст, разделив его на части по переводу строки
+            string[] text = File.ReadAllText(path).Split('\n'); 
+
+            foreach (string s in text) { 
+                //для каждой части текста проверяем наличие пробела в конце строки
+                if (s.EndsWith(" ")) {
+                    bits.Append('1'); //если он есть, то в bits добавляем '1'
+                } else {
+                    bits.Append('0'); //иначе добавляем '0'
+                }
+            }
+            
+            //вызываем функцию преобразования битов в строку и возвращаем полученный из неё результат как результат дешифрования
+            return GetStringFromBits(bits.ToString()); 
+        }
+        
+        //преобразование текста в последовательность битов
+        private static string GetBits(string text) {
+            StringBuilder result = new StringBuilder(); //здесь будет результат
+
+            foreach (byte b in Encoding.UTF8.GetBytes(text)) { //используя кодировку ASCII преобразуем текст в байты
+                //каждый байт переводим в биты (в двоичную систему счисления)
+                //а затем в тип string и добавляем в результат
+                string bits = Convert.ToString(b, 2);
+                //если длина последовательности битов меньше 8, то в двоичном представлении есть незначащие нули (например, 00010110)
+                //тогда принудительно их добавляем в начало, т.к. они могут обрезаться функций toString()
+                while (bits.Length != 8) { 
+                    //то добавляем их 
+                    bits = "0" + bits;
+                }
+                result.Append(bits); 
+            }
+
+            return result.ToString(); //возвращаем результат
+        }
+        
+        //преобразование битов в текст
+        private static string GetStringFromBits(string bits) {
+            List<byte> bytes = new List<byte>(); //здесь будут байты (байт - 8 бит)
+            
+            for (int i = 0; i + 8 < bits.Length; i += 8) { //рассматриваем каждые 8 бит
+                //отделяем 8 бит от строки битов - bits.Substring(i, 8)
+                //переводим её в байт (из двоичной системы счисления) и добавляем к списку байтов bytes
+                bytes.Add(Convert.ToByte(bits.Substring(i, 8), 2)); 
+            }
+            
+            //используя кодировку ASCII преобразуем байты в текст
+            //toArray используется для того, чтобы List<byte> привести к типу массива bytes[], который необходим
+            //функции GetString() для получения string значения данных
+            return Encoding.UTF8.GetString(bytes.ToArray()); 
+        }
+        
         public static void Main(string[] args) {
 //            //#1
 //            Task1();
@@ -290,10 +395,10 @@ namespace CryptographyTasks {
 //            Task2_1();
 //            //#2.2
 //            Task2_2();
-            //#3
-            Task3();
-            //#4
-            
+//            //#3
+//            Task3();
+//            //#4
+            Task4();
             //#5
         }
 
